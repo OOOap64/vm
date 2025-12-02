5f57b3f995888d5bdd5b4de49157f1a8825dcc88
L <l@gmail.ru>
2025-12-02 16:06:07 +0300

    web1

--- app.py
+++ app.py
@@ -1,180 +1,146 @@
 from cloudipsp import Api, Checkout
-from flask import Flask, render_template, redirect
+from flask import Flask, redirect, render_template, request, url_for
 import sqlite3
+import random
+import socket
 
+app=Flask(__name__, static_folder='static')
 
-# def ipp():
-#     host=socket.gethostname()
-#     ip=socket.gethostbyname(host)
-#     return str(ip)
-
-app = Flask(__name__, static_folder="static")
-
-# Функция для подключения к базе данных
-def get_db_connection():
-    conn = sqlite3.connect('database.db')
-    conn.row_factory = sqlite3.Row
+def condb():
+    conn=sqlite3.connect('datas.db')
+    conn.row_factory=sqlite3.Row
     return conn
 
-def init_db():
-    conn=get_db_connection()
-    conn.execute('''
-        CREATE TABLE IF NOT EXISTS goods(
-                 id INTEGER PRIMARY KEY,
-                 name VARCHAR(25),
-                 time INTEGER(2),
-                 price BIGINT,
-                 pathimg VARCHAR(100))''')
+def delete_non_paid_order():
+    conn=condb()
+    m=len(conn.execute("SELECT * FROM Comp").fetchall())
+    if m:
+        conn.execute('DELETE FROM Orders WHERE paid=Flase',)
+    conn.commit()
+    conn.close
+
+def iniDB():
+    conn=condb()
+    conn.execute("""CREATE TABLE IF NOT EXISTS Comp(id INTEGER PRIMARY KEY,
+                  name VARCHAR(50), 
+                  price INTEGER, 
+                  type VARCHAR(20),
+                  videocard VARCHAR(70), 
+                  processor VARCHAR(70),
+                  mother_curcuit VARCHAR(70),
+                  RAM VARCHAR(70),
+                  SSD VARCHAR(70),
+                  ads VARCHAR(100),
+                  img VARCHAR(30))""")
+    conn.commit()
+    conn.execute("""CREATE TABLE IF NOT EXISTS User(id INTEGER, ip VARCHAR(20), game BOOLEAN,
+                 pro BOOLEAN, office BOOLEAN, budjet BOOLEAN )""")
+    conn.commit()
+    conn.execute("""CREATE TABLE IF NOT EXISTS Orders(id INTEGER , ads VARCHAR(200) , name VARCHAR(40) , img VARCHAR(200),  num INTEGER, ip VARCHAR(20), paid BOOLEAN )""")
     conn.commit()
     conn.close()
-    # conn.execute('''CREATE TABLE IF NOT EXISTS user(
-    #              id INTEGER PRIMARY KEY,
-    #              name VARCHAR(12),
-    #              password VARCHAR(12),
-    #              email VARCHAR(30),
-    #              budjet INTEGER,
-    #              ip VARCHAR(20),
-    #              ''')   
-    # conn.commit()
-    
-# def s():
-#     us=auth()
-#     conn=get_db_connection()
-#     m=conn.execute("SELECT * FROM cart").fetchall()
-#     s=0
-#     for i in range(m):
-#         s+=float(i['price'])
 
-#     return us, s, m, conn
 
-# def auth():
-#     conn=get_db_connection()
-#     ip=ipp()
-#     users=conn.execute('SELECT * FROM user').fetchall()
-#     for i in users:
-#         if ip==i['ip']:
-#             return i
+def ipp():
+    h=socket.gethostname()
+    h=socket.gethostbyname(h)
+    return h
 
 @app.route('/')
-def home():
-    conn=get_db_connection()
-    goods=conn.execute('SELECT * FROM goods').fetchall()
-    conn.close()
-    return render_template('home.html', m=goods)
+def main():
+    delete_non_paid_order()
+    return render_template('home.html')
 
-@app.route('/about')
-def about():
-    return render_template('about.html')
+@app.route('/', methods=['POST'])
+def promocode():
+    conn=condb()
+    code=request.form['code']
+    us=conn.execute('SELECT * from User where id=?', (ipp())) 
+    if us is None:
+        conn.execute('INSERT INTO User VALUES(?, False, False, False, False)', (ipp()))
+        conn.commit()
+    if code=='playgame':
+        conn.execute('UPDATE User  SET game=True, where ip=?', (ipp()))
+        conn.commit()
+    return render_template('home.html')
 
-# @app.route("/put_away/<int:g_id>")
-# def put_away(g_id):
-#     conn=get_db_connection()
-#     us=auth()
-#     cg=[str(us['count']).split(' ')]
-#     cg[g_id-1]=str(int(cg[g_id-1])-1)
-#     st=''
-#     for i in cg:
-#         st+=i
-#     conn.execute('''UPDATE TABLE user SET good_id=?
-#                  WHERE ip=?;''', (st, ipp(),))
-#     conn.commit()
-#     conn.close()
-#     return redirect(url_for('cart'))
+@app.route('/orders')
+def order():
+    conn=condb()
+    delete_non_paid_order()
+    m=conn.execute('SELECT * FROM Order where ip=?', (ipp()))
+    return render_template('order.html', m=m)
 
-@app.route('/buy/<int:b_id>')
-def buy(b_id):
-    conn=get_db_connection()
-    a=conn.execute('SELECT * from goods').fetchall()
-    a=int(a[b_id-1]['price'])
+@app.route('/orders/away/<int:i>')
+def away(i):
+    delete_non_paid_order()
+    conn=condb()
+    conn.execute('DELETE FROM Order where id=?', (i))
+    conn.commit()
+    return render_template('order.html')
+
+@app.route('/office')
+def office():
+    conn=condb()
+    delete_non_paid_order()
+    m=conn.execute("SELECT * FROM Comp where type=office").fetchall()
+    return render_template('good.hmtl', m=m)
+
+@app.route('/game')
+def game():
+    conn=condb()
+    delete_non_paid_order()
+    m=conn.execute("SELECT * FROM Comp where type=game").fetchall()
+    return render_template('good.hmtl', m=m)
+
+@app.route('/budjet')
+def budjet():
+    conn=condb()
+    delete_non_paid_order()
+    m=conn.execute("SELECT * FROM Comp where type=budjet").fetchall()
+    return render_template('good.hmtl', m=m)
+
+@app.route('/pro')
+def pro():
+    conn=condb()
+    delete_non_paid_order()
+    m=conn.execute("SELECT * FROM Comp where type=pro").fetchall()
+    return render_template('goods.hmtl', m=m)
+
+@app.route('/good/<int:g>')
+def good(g):
+    conn=condb()
+    delete_non_paid_order()
+    m=conn.execute("SELECT * FROM Comp WHERE id=?", (g)).fetchall()[0]
+    return render_template('good.html', i=m)
+
+
+@app.route('/pay/<int:i>')
+def pay(i):
+    conn=condb()
+    delete_non_paid_order()
+    p=conn.execute('SELECT * FROM Comp where id=?', (i)).fetchall()[0]
+    disc=conn.execute(f"SELECT {p['type']} FROM User where ip=?", (ipp()))
+    url = checkout.url(data).get('checkout_url')
+    num=0 
+    ords=conn.execute('SELECT * FROM Orders, where ip=?', (ipp())).fetchall()
+    num=None
+    serch=True
+    for i in ords:
+        num=round(random.randrange(0, 999))
+        if num not in i['num']:
+            break
+    conn.execute('INSERT INTO Orderss(?, ?, ?, ?, ?, ?, ?)', (len(ords), p['ads'], p['name'], p['img'], num , ipp()))
     api = Api(merchant_id= 1396424,
               secret_key='test')
     checkout = Checkout(api=api)
     data = {
-        "currency": "USD",
-        "amount": a*100
+        "currency": "RUS",
+        "amount": 0#(float(p['price'])-float(p['price']*float(disc)*0.2))*100
     }
-    url = checkout.url(data).get('checkout_url')
-    return redirect(url)
-    # us=auth()
-    # count=[str(us['count']).split(',')]
-    # if us['id']==True:
-    #     conn=get_db_connection()
-    #     count[b_id-1]=str(int(count[b_id-1])+1)
-    #     c=''
-    #     for cc in count:
-    #         c+=cc
-    #     conn.execute('''UPDATE user SET count=?  WHERE ip=?;''', (c, ipp()))#, c, ipp()))
-    #     conn.commit()
-    #     conn.close()
+    redirect(url)
 
 
-# @app.route("/sign_in")#, methods=['POST'])
-# def r():
-#     return render_template("sign_in.html")
-
-# # def ordd():
-# #     conn=get_db_connection()
-# #     mg=conn.execute("SELECT id FROM goods").fetchall()
-# #     ord=''
-# #     for i in mg:
-# #         ord+='0 '
-# #     return ord
-
-# @app.route("/sign_in", methods=['POST'])
-# def sign_in():
-#     if request.method=='POST':
-#         nm=request.form['name']
-#         ps=request.form['password']
-#         em=request.form['email']
-#         conn=get_db_connection()
-#         m=conn.execute('SELECT * FROM user').fetchall()
-#         for i in m:
-#             if nm==i[1] and ps==i[2] and em==i[3] and ipp()==i[-1] :
-#                return redirect(url_for('home'))
-#         # ord=ordd()
-#         conn.execute('INSERT INTO user VALUES(?, ?, ?, ?, 50, ?, ?)', (int(len(m)+1), nm, ps, em, ipp()))
-#         conn.commit()
-#         conn.close()
-        
-#         return redirect(url_for('home'))
-    #return render_template(url_for('sign_in.html'))
-
-# @app.route("/cart")
-# def cart():
-#     us=auth()
-#     count=[str(us['count']).split(',')]
-#     conn=get_db_connection()
-#     s=0
-#     lo=[str(us['order_list']).split(' ')]
-#     ms=[]
-#     goods=conn.execute('SELECT * FROM goods').fetchall()
-#     if len(count)!=0:
-#         for i in range(len(goods)):
-#             if count[i]>0:
-#                 ms.append({'name':goods[i]['name'], 'sum':(goods[i]['price']*int(count[i])), 'time':goods[i]['time']})
-#     return render_template('cart.html', m=ms, us=us, sum=s, lo=lo)
-
-# @app.route("/cart", methods=['POST'])
-# def pay():
-#     us=auth()
-#     count=[str(us['count']).split(',')]
-#     conn=get_db_connection()
-#     s=0
-#     goods=conn.execute('SELECT * FROM goods').fetchall()   
-#     for d in range(len(count)):
-#         s+=int(count[d])*int(goods[d]['price'])
-#     if us["budjet"]>=s:
-#         conn.execute("UPDATE user SET  budjet=? WHERE  ip=?", (int(us['budjet'])-s, ipp()))
-#         conn.commit()
-#         conn.execute("UPDATE user SET order_list=? WHERE ip=?", (round(random.random()*10000), ipp()))
-#         conn.commit()
-#         conn.execute("UPDATE user SET count='' WHERE ip=?", (ordd(), ipp()))
-#         conn.commit()
-#         conn.close()
-#         return redirect(url_for('home'))
-#     else:
-#         return redirect(url_for('cart'))
-    
 if __name__=='__main__':
-    init_db()
-    app.run(debug=True)
+    iniDB()
+    app.run()
